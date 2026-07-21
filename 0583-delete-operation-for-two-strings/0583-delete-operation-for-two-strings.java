2e9c009ece78528296aa3201fd837be6bb03180b
Siddharth-3101 <siddharth310107@gmail.com>
2026-07-21 15:21:16 +0530

    Time: 7 ms (69.37%), Space: 46.7 MB (14.18%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -5,10 +5,6 @@ class Solution {
         int[][] ans=new int[m+1][n+1];
         for(int i=1;i<m+1;i++){
             for(int j=1;j<n+1;j++){
-                if(i==0||j==0){
-                    ans[i][j]=0;
-                    continue;
-                }
                 if(word1.charAt(i-1)==word2.charAt(j-1)){
                     ans[i][j]=1+ans[i-1][j-1];
                 }
@@ -17,7 +13,6 @@ class Solution {
                 }
             }
         }
-        int a=(m-ans[m][n])+(n-ans[m][n]);
-        return a;
+        return (m-ans[m][n])+(n-ans[m][n]);
     }
 }
