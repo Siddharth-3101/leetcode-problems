f4ac463484b852089ed3b03a393d2043da2775f2
Siddharth-3101 <siddharth310107@gmail.com>
2026-07-24 23:49:21 +0530

    Time: 160 ms (23.19%), Space: 107.2 MB (5.23%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -4,31 +4,29 @@ class Solution {
         String sub=s.substring(0,1);
         int maxLen=0;
         int[][] ans=new int[n][n];
-        for(int diff=0;diff<s.length();diff++){
-           int i=0;
-           int j=diff;
-           while(i<n && j<n){
-            if(i==j){
-                ans[i][j]=1;
-            }
-            else if(diff==1){
-                if(s.charAt(i)==s.charAt(j)){
-                    ans[i][j]=2;
+        for(int diff=0;diff<n;diff++){
+            int i=0;
+            int j=diff;
+            while(i<n && j<n){
+                if(i==j){
+                    ans[i][j]=1;
                 }
-            }
-            else{
-                if(s.charAt(i)==s.charAt(j) && ans[i+1][j-1]>0){
+                else if(diff==1){
+                    if(s.charAt(i)==s.charAt(j)){
+                        ans[i][j]=2;
+                    }
+                }
+                else if(s.charAt(i)==s.charAt(j) && ans[i+1][j-1]!=0){
                     ans[i][j]=2+ans[i+1][j-1];
                 }
+                if(j-i+1>maxLen && ans[i][j]!=0){
+                    maxLen=j-i+1;
+                    sub=s.substring(i,j+1);
+                }
+                i++;
+                j++;
             }
-            if(ans[i][j]!=0 && j-i+1>maxLen){
-                maxLen=j-i+1;
-                sub=s.substring(i,j+1);
-            } 
-            i++;
-            j++;
-           }
-                      
+
         }
         return sub;
     }
