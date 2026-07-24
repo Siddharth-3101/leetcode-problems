d65e5d7a5eaa61addb1ce6d4873013629767f79d
Siddharth-3101 <siddharth310107@gmail.com>
2026-07-24 23:52:39 +0530

    Time: 16 ms (36.62%), Space: 53.1 MB (6.1%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -3,30 +3,26 @@ class Solution {
         int n=s.length();
         int count=0;
         int[][] ans=new int[n][n];
-        for(int diff=0;diff<s.length();diff++){
-           int i=0;
-           int j=diff;
-           while(i<n && j<n){
-            if(i==j){
-                ans[i][j]=1;
-            }
-            else if(diff==1){
-                if(s.charAt(i)==s.charAt(j)){
-                    ans[i][j]=2;
+        for(int diff=0;diff<n;diff++){
+            int i=0;
+            int j=diff;
+            while(i<n && j<n){
+                if(i==j){
+                    ans[i][j]=1;
                 }
-            }
-            else{
-                if(s.charAt(i)==s.charAt(j) && ans[i+1][j-1]>0){
+                else if(diff==1){
+                    if(s.charAt(i)==s.charAt(j)){
+                    ans[i][j]=2;}
+                }
+                else if(ans[i+1][j-1]!=0 && s.charAt(i)==s.charAt(j)){
                     ans[i][j]=2+ans[i+1][j-1];
                 }
+                if(ans[i][j]!=0){
+                    count++;
+                }
+                i++;
+                j++;
             }
-            if(ans[i][j]!=0){
-                count++;
-            } 
-            i++;
-            j++;
-           }
-                      
         }
         return count;
     }
