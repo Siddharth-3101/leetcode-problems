ff9d70615a575ab9a7f103db5fbc741a0825801e
siddharth gopinath <siddharth310107@gmail.com>
2026-08-21 20:32:47 +0530

    Time: 2 ms (89.62%), Space: 44 MB (72.48%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,12 +1,12 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int[][] directions={{-1,0},{1,0},{0,-1},{0,1}};
-        Queue<int[]> queue=new LinkedList<>();
-        int n=grid.length;
-        int m=grid[0].length;
+        int m=grid.length;
+        int n=grid[0].length;
         int fresh=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        Queue<int[]> queue=new LinkedList<>();
+        int[][] direction={{-1,0},{1,0},{0,-1},{0,1}};
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
                     queue.offer(new int[]{i,j});
                 }
@@ -15,21 +15,21 @@ class Solution {
                 }
             }
         }
-        if(fresh==0){
-            return 0;
-        }
+        if(fresh==0){return 0;}
         int minutes=-1;
         while(!queue.isEmpty()){
             int size=queue.size();
             while(size!=0){
                 int[] arr=queue.poll();
-                for(int[] dir:directions){
+                for(int[] dir:direction){
                     int r=arr[0]+dir[0];
                     int c=arr[1]+dir[1];
-                    if(r<0 || r>=grid.length ||c<0 || c>=grid[0].length){continue;}
+                    if(r<0||c<0||r>=m||c>=n){
+                        continue;
+                    }
                     if(grid[r][c]==1){
-                        grid[r][c]=2;
                         fresh--;
+                        grid[r][c]=2;
                         queue.offer(new int[]{r,c});
                     }
                 }
