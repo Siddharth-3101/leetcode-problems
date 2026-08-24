3591b8c429c0ff37d86b8fd0cd8cff6335f251d2
siddharth gopinath <siddharth310107@gmail.com>
2026-08-24 11:32:21 +0530

    Time: 2 ms (89.02%), Space: 48 MB (43.08%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n=nums.length;
+        int farthest=0;
+        for(int i=0;i<n;i++){
+            if(i>farthest){
+                return false;
+            }
+            else{
+                farthest=Math.max(farthest,i+nums[i]);
+            }
+            if(farthest>=n-1){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+}
