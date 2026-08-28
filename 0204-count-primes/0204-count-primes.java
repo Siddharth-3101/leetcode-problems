2f0968e3cc638f4b086d63d27039aefeb366b634
siddharth gopinath <siddharth310107@gmail.com>
2026-08-28 10:20:44 +0530

    Time: 748 ms (7.88%), Space: 79.8 MB (10.76%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,20 +1,18 @@
 class Solution {
     public int countPrimes(int n) {
-        boolean[] isprime=new boolean[n+1];
-        Arrays.fill(isprime,true);
-        for(int i=2;i*i<n;i++){
-            if(isprime[i]){
-                for(int j=i*i ;j<n;j+=i){
-                    isprime[j]=false;
+         boolean[] arr = new boolean[n+1];
+        Arrays.fill(arr , true);
+        for(int i=2 ; i*i<n ; i++){
+            if(arr[i] == true){
+                for(int j=i*i ; j<n ; j+=i){
+                    arr[j] = false;
                 }
             }
         }
-        int count=0;
-        for(int i=2;i<n;i++){
-            if(isprime[i]){
-                count++;
-            }
+        int ans = 0;
+        for(int i=2 ; i<n ; i++){
+            if(arr[i] == true) ans++;
         }
-        return count;
+        return ans;
     }
 }
