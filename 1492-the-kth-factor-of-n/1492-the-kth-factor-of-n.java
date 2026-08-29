f21fc0a1b9dad18873bf2d01cdedd16c14898267
siddharth gopinath <siddharth310107@gmail.com>
2026-08-29 13:48:32 +0530

    Time: 0 ms (100%), Space: 42.3 MB (53.28%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
@@ -1,10 +1,21 @@
 class Solution {
     public int kthFactor(int n, int k) {
-        for(int i=1;i<=n;i++){
+        for(int i=1;i*i<=n;i++){
             if(n%i==0){
                 k--;
                 if(k==0){
                     return i;
+                }
+            }
+        }
+        for(int i=(int) Math.sqrt(n);i>=1;i--){
+            if(n%i==0){
+                int pair=n/i;
+                if(pair!=i){
+                    k--;
+                    if(k==0){
+                    return pair;
+                    }
                 }
             }
         }
