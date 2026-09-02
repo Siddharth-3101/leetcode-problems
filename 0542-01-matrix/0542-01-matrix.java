debc5acd76e0f92126ca6d953e53a032dc0be58c
siddharth gopinath <siddharth310107@gmail.com>
2026-09-02 12:30:50 +0530

    Time: 17 ms (19.13%), Space: 49.3 MB (36.43%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,15 +1,15 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int[][] direction={{-1,0},{1,0},{0,1},{0,-1}};
-        int n=mat.length;
-        int m=mat[0].length;
-        int[][] dist=new int[n][m];
-        for(int i = 0; i < n; i++){
-            Arrays.fill(dist[i], -1);
+        int[][] directions={{0,-1},{0,1},{1,0},{-1,0}};
+        int m=mat.length;
+        int n=mat[0].length;
+        int[][] dist=new int[m][n];
+        for(int i=0;i<m;i++){
+            Arrays.fill(dist[i],-1);
         }
         Queue<int[]> queue=new LinkedList<>();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(mat[i][j]==0){
                     dist[i][j]=0;
                     queue.offer(new int[]{i,j});
@@ -18,10 +18,10 @@ class Solution {
         }
         while(!queue.isEmpty()){
             int[] arr=queue.poll();
-            for(int[] dir:direction){
+            for(int[] dir:directions){
                 int r=arr[0]+dir[0];
                 int c=arr[1]+dir[1];
-                if(r<0 || c<0 ||r>=n ||c>=m){continue;}
+                if(r<0 ||c<0||r>=m||c>=n){continue;}
                 if(dist[r][c]==-1){
                     dist[r][c]=dist[arr[0]][arr[1]]+1;
                     queue.offer(new int[]{r,c});
