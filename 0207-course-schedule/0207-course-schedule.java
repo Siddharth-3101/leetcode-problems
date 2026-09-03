3b8ecebb6d4c3e1faa5fa27b50b7d2bf9d11989a
siddharth gopinath <siddharth310107@gmail.com>
2026-09-03 09:33:35 +0530

    Time: 7 ms (56.53%), Space: 47.2 MB (48.39%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,15 +1,16 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
-        int[] indegree=new int[numCourses];
+        List<List<Integer>> adj=new ArrayList<>();
         Queue<Integer> queue=new LinkedList<>();
+        int count=0;
+        int[] indegree=new int[numCourses];
         for(int i=0;i<numCourses;i++){
             adj.add(new ArrayList<>());
         }
-        for(int[] pre:prerequisites){
-            int course=pre[0];
-            int prerequisite=pre[1];
-            adj.get(prerequisite).add(course);
+        for(int i=0;i<prerequisites.length;i++){
+            int course=prerequisites[i][0];
+            int prereq=prerequisites[i][1];
+            adj.get(prereq).add(course);
             indegree[course]++;
         }
         for(int i=0;i<numCourses;i++){
@@ -17,11 +18,10 @@ class Solution {
                 queue.offer(i);
             }
         }
-        int count=0;
         while(!queue.isEmpty()){
-            int course=queue.poll();
+            int c=queue.poll();
             count++;
-            for(int next:adj.get(course)){
+            for(int next:adj.get(c)){
                 indegree[next]--;
                 if(indegree[next]==0){
                     queue.offer(next);
