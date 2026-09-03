0ad61db7ee6977100bb1dbbf71793010d5f8af09
siddharth gopinath <siddharth310107@gmail.com>
2026-09-03 09:51:57 +0530

    Time: 2 ms (89.01%), Space: 44 MB (72.07%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,41 +1,43 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int[][] direction={{-1,0},{1,0},{0,-1},{0,1}};
-        int m=grid.length;
-        int n=grid[0].length;
-        Queue<int[]> queue=new LinkedList<>();
-        int fresh=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1){
-                    fresh++;
-                }
-                else if(grid[i][j]==2){
-                    queue.offer(new int[]{i,j});
-                }
+       int m=grid.length;
+       int n=grid[0].length;
+       int[][] direction={{1,0},{-1,0},{0,1},{0,-1}};
+       int fresh=0;
+       Queue<int[]> queue=new LinkedList<>();
+       for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(grid[i][j]==1){
+                fresh++;
+            }
+            else if(grid[i][j]==2){
+                queue.offer(new int[]{i,j});
             }
         }
-        if(fresh==0){return 0;}
-        int minutes=-1;
-        while(!queue.isEmpty()){
-            int size=queue.size();
-            while(size!=0){
-                int[] arr=queue.poll();
-                for(int[] dir:direction){
-                    int r=arr[0]+dir[0];
-                    int c=arr[1]+dir[1];
-                    if(r<0||c<0||r>=m||c>=n){continue;}
-                    if(grid[r][c]==1){
-                        grid[r][c]=2;
-                        queue.offer(new int[]{r,c});
-                        fresh--;
-                    }
+       }
+       if(fresh==0){
+        return 0;
+       }
+       int minutes=-1;
+       while(!queue.isEmpty()){
+        int size=queue.size();
+        while(size!=0){
+            int[]arr=queue.poll();
+            for(int[] dir:direction){
+                int r=arr[0]+dir[0];
+                int c=arr[1]+dir[1];
+                if(r<0||c<0||r>=m||c>=n){continue;}
+                if(grid[r][c]==1){
+                    fresh--;
+                    grid[r][c]=2;
+                    queue.offer(new int[]{r,c});
                 }
-                size--;
             }
-            minutes++;
+            size--;
         }
-        if(fresh>0){return -1;}
-        return minutes;
+        minutes++;
+       }
+       if(fresh>0){return -1;}
+       return minutes;
     }
 }
