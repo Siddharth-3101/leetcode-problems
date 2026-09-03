5e3fd9a1f406ee61f46e2ac24ea51e1763a4579c
siddharth gopinath <siddharth310107@gmail.com>
2026-09-03 09:42:22 +0530

    Time: 4 ms (46.71%), Space: 52.3 MB (58.32%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,26 +1,24 @@
 class Solution {
-    int[][] directions={{-1,0},{1,0},{0,-1},{0,1}};
+    int[][] direction={{0,1},{0,-1},{1,0},{-1,0}};
     public void dfs(char[][] grid,boolean[][] visited,int row,int col){
-        if(row < 0 || row >= grid.length || col < 0 || col >= grid[0].length ||visited[row][col] ||grid[row][col] == '0'){
-            return;
-        }
+        if(row<0||col<0||row>=grid.length||col>=grid[0].length||grid[row][col]=='0'||visited[row][col]){return;}
         visited[row][col]=true;
-        for(int[] dir:directions){
+        for(int[] dir:direction){
             int r=row+dir[0];
             int c=col+dir[1];
             dfs(grid,visited,r,c);
-        }    
+        }
     }
     public int numIslands(char[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
-        boolean[][] visited=new boolean[n][m];
+        int m=grid.length;
+        int n=grid[0].length;
+        boolean[][] visited=new boolean[m][n];
         int island=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]=='1' && !visited[i][j]){
+                    dfs(grid,visited,i,j);
                     island++;
-                dfs(grid,visited,i,j);
                 }
             }
         }
